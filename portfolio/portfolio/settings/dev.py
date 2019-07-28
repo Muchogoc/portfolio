ebdb788bce1bd8e421c6ae5fa490fc7dbb558edf
Charles Muchogo <muchogoc@gmail.com>
2019-07-28 18:25:12 +0300

    temporarily allowing all hosts

--- portfolio/portfolio/settings/dev.py
+++ portfolio/portfolio/settings/dev.py
@@ -18,7 +18,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = True
 
 # ALLOWED_HOSTS = [env.list('ALLOWED_HOSTS', cast=None)]
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '52.14.224.154']
+ALLOWED_HOSTS = ['*']
 
 PROMETHEUS_EXPORT_MIGRATIONS = False
 
